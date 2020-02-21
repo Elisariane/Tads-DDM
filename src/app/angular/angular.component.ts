0d211d3d80973defe537c37b353d52b3215f5394
Elisariane <elisarianeBS@gmail.com>
2020-02-20 22:20:09 -0300

    oi

---
+++ src/app/angular/angular.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector:"angular-component",
+  templateUrl:"./angular.component.html",
+  styleUrls:["./angular.component.css"]
+})
+
+export class AngularComponent{}
